031a4730e6c1604dfa412c9c49713e37344ee1f2
Michael Koegel <mkoegel@gmail.com>
2018-01-23 15:38:01 -0500

    Cleaned up excess code that I had commented out.

--- spacex.py
+++ spacex.py
@@ -16,41 +16,11 @@ def return_next_launch():
 
     # setting up our list variables. This is because SpaceX knows about the next ~3 launches,
     # but only provides full details of the next launch.
-    flight_numbers = []
     launch_dates = []
-    rocket_names = []
-    first_stages_reused = []
-    second_stages_reused = []
-    payload_ids = []
-    payload_contents = []
-    payload_weight = []
-    customer = []
-    launch_sites = []
     launch_times = []
     spacex = []
 
     count = 0
-
-    # This works. Just not sure of the best way to use this. Namedtuples or several lists
-    """
-    # There has to be a better way. I thought about namedtuples...
-    while count < len(spacex_data):
-        flight_numbers.append(spacex_data[count]["flight_number"])
-        launch_dates.append(spacex_data[count]["launch_date_local"])
-        rocket_names.append(spacex_data[count]["rocket"]["rocket_name"])
-        first_stages_reused.append(spacex_data[count]["rocket"]["first_stage"]["cores"][0]["reused"])
-        second_stages_reused.append(spacex_data[count]["rocket"]["second_stage"]["payloads"][0]["reused"])
-        payload_ids.append(spacex_data[count]["rocket"]["second_stage"]["payloads"][0]["payload_id"])
-        payload_contents.append(spacex_data[count]["rocket"]["second_stage"]["payloads"][0]["payload_type"])
-        # Need to figure out how to handle entries that are blank
-        if spacex_data[count]["rocket"]["second_stage"]["payloads"][0]["payload_mass_kg"] is None:
-            payload_weight.append("No weight provided")
-        else:
-            payload_weight.append(spacex_data[count]["rocket"]["second_stage"]["payloads"][0]["payload_mass_kg"])
-        customer.append(spacex_data[count]["rocket"]["second_stage"]["payloads"][0]["customers"])
-        launch_sites.append(spacex_data[count]["launch_site"]["site_name_long"])
-        count += 1
-    """
 
     date_count = 0
     while date_count < len(spacex_data):
@@ -93,51 +63,7 @@ def return_next_launch():
                              )
                       )
         count += 1
-    """
-    # time / date info come in ISO 8601 format - 2018-01-07T20:00:00-05:00
 
-    # Currently I am not using this. When I run the program here, I only get a single date & time
-    # added to launches. When I run the program separately (line by line), it works fine. Need to
-    # troubleshoot it more
-    for times in launch_dates:
-        try:
-            ts = time.strptime(times[:19], "%Y-%m-%dT%H:%M:%S")
-            launch_times.append(time.strftime("%Y-%m-%d @ %H:%M:%S", ts))
-        except ValueError:
-            continue
-
-        # Not sure if there is a better way to return two sets of launch info.
-        # These will go into a slack room chanel and I don't want it horizontally displayed
-        flight_count = -1  # starting with -1. Not sure how to start with 0 when all I need to do is return data
-
-        # If I don't split this out, when data is returned it crashes the application.
-        # Sometimes the API has multiple launches. Other times it only has one.
-        # Could I create a function to clean it up?
-        if len(flight_numbers) > 1:  # this will return info for two launches if they are present in the API
-            while flight_count < 2:
-                flight_count += 1  # I don't know if there is around this...
-                return f"Flight Number: {flight_numbers[flight_count]}\n" \
-                       f"Launch Date: {launch_dates[flight_count]}\n" \
-                       f"Rocket Type: {rocket_names[flight_count]}\n" \
-                       f"First Stage Rocket Reused: {first_stages_reused[flight_count]}\n" \
-                       f"Second Stage Rocket Reused: {second_stages_reused[flight_count]}\n" \
-                       f"Payload: {payload_ids[flight_count]}\n" \
-                       f"Payload Contents: {payload_contents[flight_count]}\n" \
-                       f"Payload Weight: {payload_weight[flight_count]}\n" \
-                       f"Customer: {customer[flight_count][0]}\n" \
-                       f"Launch Site: {launch_sites[flight_count]}\n\n"
-        else:  # This will print if the API only contains one set of launch data
-            return f"Flight Number: {flight_numbers[0]}\n" \
-                   f"Launch Date: {launch_dates[0]}\n" \
-                   f"Rocket Type: {rocket_names[0]}\n" \
-                   f"First Stage Rocket Reused: {first_stages_reused[0]}\n" \
-                   f"Second Stage Rocket Reused: {second_stages_reused[0]}\n" \
-                   f"Payload: {payload_ids[0]}\n" \
-                   f"Payload Contents: {payload_contents[0]}\n" \
-                   f"Payload Weight: {payload_weight[flight_count]}\n" \
-                   f"Customer: {customer[0][0]}\n" \
-                   f"Launch Site: {launch_sites[0]}\n\n"
-    """
     return f"{spacex[0]}\n" \
            f"{spacex[1]}\n" \
            f"{spacex[2]}\n"
