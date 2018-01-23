ce7508dbcf1038506734c42dc989b2270ec4527d
Michael Koegel <mkoegel@gmail.com>
2018-01-23 15:45:27 -0500

    added comments

--- spacex.py
+++ spacex.py
@@ -20,20 +20,22 @@ def return_next_launch():
     launch_times = []
     spacex = []
 
-    count = 0
-
-    date_count = 0
+    date_count = 0  # this is to loop through the while loop in our launch_dates list
     while date_count < len(spacex_data):
         launch_dates.append(spacex_data[date_count]["launch_date_local"])
         date_count += 1
 
+    # This will convert our ISO date time format to human readable material (Year-Month-Date @ Hour:Minute:Second)
+    # For example, 2018-01-23 @ 13:00:00
     for times in launch_dates:
         try:
-            ts = time.strptime(times[:19], "%Y-%m-%dT%H:%M:%S")
-            launch_times.append(time.strftime("%Y-%m-%d @ %H:%M:%S", ts))
-        except ValueError:
+            ts = time.strptime(times[:19], "%Y-%m-%dT%H:%M:%S")  # need to use strptime
+            launch_times.append(time.strftime("%Y-%m-%d @ %H:%M:%S", ts))  # appending our formatted content to the list
+        except ValueError:  # I've only run into this error once and it may have been a fluke, being safe
             continue
 
+    # Creating a namedtuple to store our data. Eventually, I need to learn how to print properly formatted
+    # data for the 3 flights listed.
     Spacex = collections.namedtuple("SpaceX",
                                     "Flight_Number "
                                     "Launch_Date "
@@ -49,6 +51,7 @@ def return_next_launch():
 
     # I'm not sure if this is the best way. I can't alter these tuple values (that I know of).
     # I also don't know how to return multiple entries which is why I went this way.
+    count = 0  # This will help us change the count for the while loop below
     while count < len(spacex_data):
         spacex.append(Spacex(spacex_data[count]["flight_number"],
                              launch_times[count],
@@ -64,6 +67,8 @@ def return_next_launch():
                       )
         count += 1
 
+    # I know this is a cryptic return statement. If I could figure out how to loop through a return statement
+    # I'd use something like spacex[0].flight_number so it is easier to read
     return f"{spacex[0]}\n" \
            f"{spacex[1]}\n" \
            f"{spacex[2]}\n"
