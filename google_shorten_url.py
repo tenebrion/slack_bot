a60e822d22e53e1cef9e1c54298a069a3c4dbbe7
Michael Koegel <mkoegel@gmail.com>
2018-02-12 18:46:18 -0500

    Calling the new function to strip out bad characters

--- google_shorten_url.py
+++ google_shorten_url.py
@@ -1,22 +1,11 @@
 import json
 import requests
+import remove_chars
 from misc import apis
 
 # Setting up initial variables
 API_KEY = apis.google()
 FULL_URL = f"https://www.googleapis.com/urlshortener/v1/url?key={API_KEY}"
-bad_chars = ["<", ">"]
-
-
-def remove_extra_chars(text):
-    """
-    This simple function will clean up excess characters
-    :param text:
-    :return:
-    """
-    for char in bad_chars:
-        if char in text:
-            return text.replace(char, "")
 
 
 def return_shorter_url(url):
@@ -26,10 +15,9 @@ def return_shorter_url(url):
     :return:
     """
     # found out that the entries were coming over in this format: <http://www.someurl.com>
-    fixed_url = remove_extra_chars(url)
+    fixed_url = remove_chars.clean_text(url)
     payload = {"longUrl": fixed_url}
     headers = {"content-type": "application/json"}
     # making a post to google API
     r = requests.post(FULL_URL, data=json.dumps(payload), headers=headers).json()
     return f"Short URL: {r['id']}"
-
