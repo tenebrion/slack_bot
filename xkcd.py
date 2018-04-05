b413f742e42b3e11e152487cdada3ab38516155f
Michael Koegel <mkoegel@gmail.com>
2018-04-05 11:15:49 -0400

    Moved URL from global scope to local scope. Added comments.

--- xkcd.py
+++ xkcd.py
@@ -1,21 +1,20 @@
 import get_json_data
 
-PARTIAL_URL = "http://xkcd.com/"
-END_URL = "info.0.json"
-
 
 def return_xkcd_img(comic=None):
     """
     Simple method to return current xkcd comic strip
     The user can grab the daily image or specify a comic number to read
     :param comic:
-    :return:
+    :return: img['img']
     """
+    partial_url = "http://xkcd.com/"
+    end_url = "info.0.json"
     if comic is None:
-        full_url = PARTIAL_URL + END_URL  # building url
+        full_url = partial_url + end_url  # building url
         img = get_json_data.grab_json_data(full_url)
     else:
         comic_number = comic + "/"  # creating the comic number url
-        full_comic_url = PARTIAL_URL + comic_number + END_URL
+        full_comic_url = partial_url + comic_number + end_url
         img = get_json_data.grab_json_data(full_comic_url)
     return img["img"]  # only want to return image of the comic
