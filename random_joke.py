527b859016a19bc1218a553191a2af47e69bf7bd
Michael Koegel <mkoegel@gmail.com>
2018-04-05 11:07:24 -0400

    Moved URL from global scope to local scope. Also added comments

--- random_joke.py
+++ random_joke.py
@@ -1,16 +1,15 @@
 import remove_chars
 import get_json_data
 
-# URL for random joke
-URL = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
-
 
 def return_joke():
     """
     Simple method to pull a random joke
-    :return:
+    :return: setup, punchline
     """
-    data = get_json_data.grab_json_data(URL)  # pulling data (json format)
+    # URL for random joke
+    url = "https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke"
+    data = get_json_data.grab_json_data(url)  # pulling data (json format)
     setup = remove_chars.clean_text(data["setup"])
     punchline = remove_chars.clean_text(data["punchline"])
     return f"{setup}\n" \
