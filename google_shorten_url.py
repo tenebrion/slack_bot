4c8e3086d0209df46c40ec1d1fa53ad4ce4bf5a7
Michael Koegel <mkoegel@gmail.com>
2018-04-05 11:00:21 -0400

    Removed URL from global scope to local scope.

--- google_shorten_url.py
+++ google_shorten_url.py
@@ -5,7 +5,6 @@ from misc import apis
 
 # Setting up initial variables
 API_KEY = apis.google()
-FULL_URL = f"https://www.googleapis.com/urlshortener/v1/url?key={API_KEY}"
 
 
 def return_shorter_url(url):
@@ -15,9 +14,10 @@ def return_shorter_url(url):
     :return:
     """
     # found out that the entries were coming over in this format: <http://www.someurl.com>
+    full_url = f"https://www.googleapis.com/urlshortener/v1/url?key={API_KEY}"
     fixed_url = remove_chars.clean_text(url)
     payload = {"longUrl": fixed_url}
     headers = {"content-type": "application/json"}
     # making a post to google API
-    r = requests.post(FULL_URL, data=json.dumps(payload), headers=headers).json()
+    r = requests.post(full_url, data=json.dumps(payload), headers=headers).json()
     return f"Short URL: {r['id']}"
